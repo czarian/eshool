4d3620a0b5d7c350a15a973997638fadd413f58f
Czarek <karasoi@poczta.onet.pl>
2017-01-10 18:22:11 +0100

    use load_and_authorize_resource

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -3,25 +3,28 @@ module API
     class QuestionsController < ApplicationController
       before_filter :authenticate_user!
       skip_before_action :verify_authenticity_token
-      before_action :set_question, only: [:show, :update]
+      #before_action :set_question, only: [:show, :update]
+      load_and_authorize_resource
 
       clear_respond_to
       respond_to :json
 
       def index
-        @questions = Question.get_by_exam_id params[:exam_id]
+        #@questions = Question.get_by_exam_id params[:exam_id]
 
-        authorize! :read, @questions
+        #authorize! :read, @questions
         if @questions.count === 0
           render :status => 404, :json => {
             :errors => "Questions not found"
           }.to_json
+        else
+          render "index"
         end
       end
 
       def create
-        @question = Question.new(question_params)
-        authorize! :create, @question
+        #@question = Question.new(question_params)
+        #authorize! :create, @question
         if @question.save
           render @question, notice: 'Qusetion added'
         else
@@ -30,15 +33,14 @@ module API
       end
 
       def update
-        @find_question.update(question_params)
-        authorize! :update, @find_question
-        render @find_question
+        @question.update(question_params)
+        render @question
       end
 
       private
 
       def set_question
-        @find_question = Question.find(params[:id])
+        @question = Question.find(params[:id])
       end
 
       def question_params
